b11a79abdc77624b4750912267794efd20e3ea1d
Bailey <tsby@gft.com>
2019-12-27 17:02:39 +0000

    Fixing resolved conflicts

--- tb-eagle-console-ui/src/app/app.module.ts
+++ tb-eagle-console-ui/src/app/app.module.ts
@@ -1,13 +1,8 @@
-import { RouterModule } from '@angular/router';
-import { LoginModule } from './login/login.module';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
 import { AppComponent } from './app.component';
-
-import { SidebarModule } from './sidebar/sidebar.module';
-import { AppRoutingModule } from './app-routing.module';
-
 
 const routes: Routes = [
   {
@@ -17,8 +12,7 @@ const routes: Routes = [
     path: 'dashboard',
     loadChildren: () => import('./dashboards/dashboards.module').then(m => m.DashboardsModule)
   }
-];
-
+]
 
 @NgModule({
   declarations: [
@@ -26,10 +20,6 @@ const routes: Routes = [
   ],
   imports: [
     BrowserModule,
-
-    SidebarModule,
-    AppRoutingModule,
-    LoginModule,
     RouterModule.forRoot(routes)
   ],
   providers: [],
