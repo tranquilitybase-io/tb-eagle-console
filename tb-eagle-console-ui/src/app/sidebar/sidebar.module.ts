f7e7af5d245385a8cb84682679d65eec9fd2c4b3
Mariusz <mariusz.sielski@gft.com>
2019-12-30 12:22:20 +0100

    sidebar finished

--- tb-eagle-console-ui/src/app/sidebar/sidebar.module.ts
+++ tb-eagle-console-ui/src/app/sidebar/sidebar.module.ts
@@ -5,12 +5,20 @@ import { SharedModule } from '@app/shared/shared.module';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { IconsBarComponent } from './icons-bar/icons-bar.component';
 import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
+import { UserComponent } from './user/user.component';
+import { OrganizationComponent } from './organization/organization.component';
+import { SectionHeaderComponent } from './section-header/section-header.component';
+import { NavItemComponent } from './nav-item/nav-item.component';
 
 @NgModule({
   declarations: [
     SidebarComponent,
     IconsBarComponent,
-    NavigationBarComponent
+    NavigationBarComponent,
+    UserComponent,
+    OrganizationComponent,
+    SectionHeaderComponent,
+    NavItemComponent
   ],
   imports: [
     CommonModule,
