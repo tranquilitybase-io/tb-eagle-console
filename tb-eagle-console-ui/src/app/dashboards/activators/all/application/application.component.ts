22c202a57eb88eba19a4bbf845ccda25400862ec
Mariusz <mariusz.sielski@gft.com>
2020-01-03 12:53:55 +0100

    wip

--- tb-eagle-console-ui/src/app/dashboards/activators/all/application/application.component.ts
+++ tb-eagle-console-ui/src/app/dashboards/activators/all/application/application.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
+
+import { Application } from '../interfaces';
 
 @Component({
   selector: 'app-application',
   templateUrl: './application.component.html',
   styleUrls: ['./application.component.scss']
 })
-export class ApplicationComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+export class ApplicationComponent {
+  @Input() app: Application;
 }
