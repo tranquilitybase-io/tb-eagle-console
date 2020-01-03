2767aae30b617f36053a4d971b443d263705166a
sandyasheri-gft <58600085+sandyasheri-gft@users.noreply.github.com>
2020-01-03 18:01:39 +0000

    Issue#85
    Added category bar

--- tb-eagle-console-ui/src/app/dashboards/solutions/solution-select/solution-select.component.ts
+++ tb-eagle-console-ui/src/app/dashboards/solutions/solution-select/solution-select.component.ts
@@ -1,11 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-solution-select',
   templateUrl: './solution-select.component.html',
   styleUrls: ['./solution-select.component.scss']
 })
+
 export class SolutionSelectComponent implements OnInit {
+  
+  @Input('values') values: { name: string, count: number }[];
 
   constructor() { }
 
--- tb-eagle-console-ui/src/app/dashboards/solutions/solutions/solutions.component.ts
+++ tb-eagle-console-ui/src/app/dashboards/solutions/solutions/solutions.component.ts
@@ -6,23 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./solutions.component.scss']
 })
 export class SolutionsComponent implements OnInit {
-
-  // private solutions = [
-  //   {name:'Back-Office settkements',
-  //     desc:'Solution for internal users to be able to process settlement for FX transactions',
-  //     details :{applications :5, teams:6, lastUpdated:''}},
-  //   {name:'Risk Engine',
-  //     desc:'Solution to internal users with portfolio view of trading products ',
-  //     details :{applications :4, teams:1, lastUpdated:''}},
-  //   ];
-
+  
   private solutions = [
     new Solution('Back-Office Settlements',
       'Solution for internal users to be able to process settlement for FX transactions',5, 2, '2009'),
     new Solution('Risk Engine',
       'Solution for internal users to be able to process settlement for FX transactions',3, 1, '2010'),
+    new Solution('Risk Engine',
+      'Solution for internal users to be able to process settlement for FX transactions',3, 1, '2010'),
     new Solution('Back-Office Settlements',
       'Solution for internal users to be able to process settlement for FX transactions',9, 3, '2018')
+];
+private values= [
+  {name: "Favourites", count: 10 },
+  {name: "Active", count: 4 },
+  {name: "Archived", count: 3 }
 ];
   
 
