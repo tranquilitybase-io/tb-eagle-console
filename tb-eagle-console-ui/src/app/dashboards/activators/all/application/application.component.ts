ce66fa12e20635b0a3bb218369725254e54bf2f3
Mariusz <mariusz.sielski@gft.com>
2020-01-08 08:19:15 +0100

    activators interaction work in progress

--- tb-eagle-console-ui/src/app/dashboards/activators/all/application/application.component.ts
+++ tb-eagle-console-ui/src/app/dashboards/activators/all/application/application.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, HostListener } from '@angular/core';
 import getCustomProperty from '@app/shared/utils/getCustomProperty';
 
 import { Application } from '../interfaces';
@@ -10,10 +10,21 @@ import { Application } from '../interfaces';
 })
 export class ApplicationComponent {
   strokeColor = getCustomProperty('--grey');
+  active = false;
 
   @Input() app: Application;
 
   isRestricted() {
     return this.app.sensitivity.toLowerCase() === 'restricted';
+  }
+
+  @HostListener('mouseover')
+  onMouseOver() {
+    this.active = true;
+  }
+
+  @HostListener('mouseleave')
+  onMouseLeave() {
+    this.active = false;
   }
 }
