010652741bc12a587e52ead1d1d492b66e5348c2
Bailey <tsby@gft.com>
2020-01-09 16:37:05 +0000

    Further changes to solution creation wizard view; work left to refine accompanying css

--- tb-eagle-console-ui/src/app/dashboards/solutions/create-solution/create-solution.component.ts
+++ tb-eagle-console-ui/src/app/dashboards/solutions/create-solution/create-solution.component.ts
@@ -9,22 +9,31 @@ import { Component, OnInit } from '@angular/core';
 export class CreateSolutionComponent implements OnInit {
 
 
-
+  // drop down temporary values
   businessUnits: string[];
+  ciOptions: string[];
+  cdOptions: string[];
+  sourceControlOptions: string[];
+  changeControlOptions: string[];
 
-  onPartTwo: boolean = true;
-
-  constructor() { }
+  onPartTwo: boolean = false;
+  screenNum: number = 0;
 
   ngOnInit() {
 
     this.businessUnits = ['FICC', 'Modern Apps', 'Data'];
+    this.ciOptions = ['Jenkins CI', 'Bamboo', 'Travis CI'];
+    this.cdOptions = ['Jenkins', 'Spinnaker', 'Screwdriver' ];
+    this.sourceControlOptions = ['GitHub', 'ButBucket'];
+    this.changeControlOptions = ['Service Now'];
 
   }
 
   toggleSolutionPage(){
 
     this.onPartTwo = !this.onPartTwo;
+
+    this.screenNum = this.screenNum == 0 ? this.screenNum = 1 : this.screenNum = 0;
 
   }
 
