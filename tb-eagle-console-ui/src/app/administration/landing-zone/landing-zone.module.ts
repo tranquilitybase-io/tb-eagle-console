de309ff054b8bb9a5222e1d4c6a5e65f61867178
Fábio Andrey Salles <fyss@gft.com>
2020-03-12 17:42:08 -0300

    Landing Zone - Setup Progress Expansion Panel

--- tb-eagle-console-ui/src/app/administration/landing-zone/landing-zone.module.ts
+++ tb-eagle-console-ui/src/app/administration/landing-zone/landing-zone.module.ts
@@ -9,11 +9,14 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatButtonToggleModule } from '@angular/material/button-toggle';
 import { MatCardModule } from '@angular/material/card';
 import { MatDividerModule } from '@angular/material/divider';
+import { MatExpansionModule } from '@angular/material/expansion';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatStepperModule } from '@angular/material/stepper';
 import { LayoutModule } from '@angular/cdk/layout';
+import { STEPPER_GLOBAL_OPTIONS } from '@angular/cdk/stepper';
 
 const routes: Routes = [
   {
@@ -32,11 +35,19 @@ const routes: Routes = [
     MatButtonToggleModule,
     MatCardModule,
     MatDividerModule,
+    MatExpansionModule,
     MatGridListModule,
     MatIconModule,
     MatMenuModule,
     MatProgressSpinnerModule,
+    MatStepperModule,
     LayoutModule
+  ],
+  providers: [
+    {
+      provide: STEPPER_GLOBAL_OPTIONS,
+      useValue: { displayDefaultIndicatorType: false }
+    }
   ]
 })
 export class LandingZoneModule {}
