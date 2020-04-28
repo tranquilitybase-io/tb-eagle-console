b76ef9f7c9d9392222fc1f9a804184690315efdc
kwicherska <katarzyna.wicherska@gft.com>
2020-04-28 16:00:00 +0200

    update of modules in unit test

--- src/app/mission-control/solutions/solutions-view-edit/solutions-view-edit.component.spec.ts
+++ src/app/mission-control/solutions/solutions-view-edit/solutions-view-edit.component.spec.ts
@@ -1,13 +1,39 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { SolutionsViewEditComponent } from './solutions-view-edit.component';
+import {
+  MatFormFieldModule,
+  MatSelectModule,
+  MatIconModule,
+  MatOptionModule,
+  MatListModule,
+  MatInputModule
+} from '@angular/material';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { provideMockStore, MockStore } from '@ngrx/store/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('SolutionsViewEditComponent', () => {
   let component: SolutionsViewEditComponent;
   let fixture: ComponentFixture<SolutionsViewEditComponent>;
-
+  let store: MockStore<any>;
+  const initialState = true;
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [SolutionsViewEditComponent]
+      declarations: [SolutionsViewEditComponent],
+      imports: [
+        RouterTestingModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatSelectModule,
+        MatOptionModule,
+        MatListModule,
+        MatIconModule,
+        FormsModule,
+        ReactiveFormsModule,
+        BrowserAnimationsModule
+      ],
+      providers: [provideMockStore({ initialState })]
     }).compileComponents();
   }));
 
