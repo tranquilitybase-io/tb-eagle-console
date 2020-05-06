2f8a627f135d651d35fc85198885351e95c19c54
kwicherska <katarzyna.wicherska@gft.com>
2020-05-06 11:10:51 +0200

    unit test update

--- src/app/administration/landing-zone/landing-zone-wan/landing-zone-wan-view/landing-zone-wan-view.component.spec.ts
+++ src/app/administration/landing-zone/landing-zone-wan/landing-zone-wan-view/landing-zone-wan-view.component.spec.ts
@@ -1,14 +1,32 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LandingZoneWanViewComponent } from './landing-zone-wan-view.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import { MatIconModule, MatGridListModule, MatCardModule, MatChipsModule, MatListModule } from '@angular/material';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { provideMockStore, MockStore } from '@ngrx/store/testing';
 
 describe('LandingZoneWanViewComponent', () => {
   let component: LandingZoneWanViewComponent;
   let fixture: ComponentFixture<LandingZoneWanViewComponent>;
-
+  let store: MockStore<any>;
+  const initialState = true;
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [LandingZoneWanViewComponent]
+      declarations: [LandingZoneWanViewComponent],
+      imports: [
+        RouterTestingModule,
+        MatIconModule,
+        FormsModule,
+        ReactiveFormsModule,
+        BrowserAnimationsModule,
+        MatGridListModule,
+        MatCardModule,
+        MatChipsModule,
+        MatListModule
+      ],
+      providers: [provideMockStore({ initialState })]
     }).compileComponents();
   }));
 
