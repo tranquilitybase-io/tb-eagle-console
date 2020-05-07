2b57ef9f8d0fe7534a1e9f6c5dd5a86b7e138354
Rafal Mucha <r.p.mucha@gmail.com>
2020-05-07 11:33:33 +0200

    Deleted doubled vars from app, made sidenav opened

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,4 @@
 import { Component } from '@angular/core';
-import { Observable } from 'rxjs';
-import { User } from '@app/login/login.model';
-import { Store, select } from '@ngrx/store';
-import { selectUserIsAdmin, selectIsAuthenticated } from '@app/login/login.reducer';
 
 @Component({
   selector: 'app-root',
@@ -11,13 +7,6 @@ import { selectUserIsAdmin, selectIsAuthenticated } from '@app/login/login.reduc
 })
 export class AppComponent {
   title = 'tb-eagle-console-ui';
-  userIsAdmin$: Observable<User>;
-  isAuthenticated$: Observable<boolean>;
 
-  constructor(private store: Store<any>) {}
-
-  ngOnInit() {
-    this.userIsAdmin$ = this.store.pipe(select(selectUserIsAdmin));
-    this.isAuthenticated$ = this.store.pipe(select(selectIsAuthenticated));
-  }
+  ngOnInit() {}
 }
