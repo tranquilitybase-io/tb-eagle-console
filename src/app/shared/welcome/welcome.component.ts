87ff77abf845a474f8028adeff5c0c2ceef0fc8f
Rafal Mucha <r.p.mucha@gmail.com>
2020-05-27 00:21:20 +0200

    Fixed the modal showing diff text based on role

--- src/app/shared/welcome/welcome.component.ts
+++ src/app/shared/welcome/welcome.component.ts
@@ -3,23 +3,31 @@ import { MatDialog } from '@angular/material/dialog';
 import { Observable } from 'rxjs';
 import { User } from '@app/login/login.model';
 import { Store, select } from '@ngrx/store';
-import { selectUserIsAdmin, selectIsAuthenticated } from '@app/login/login.reducer';
+import { selectUserIsAdmin } from '@app/login/login.reducer';
 
 @Component({
   selector: 'welcome-dialog',
   templateUrl: 'welcome-dialog.component.html',
   styleUrls: ['welcome.component.scss']
 })
-export class WelcomeComponent {}
+export class WelcomeComponent implements OnInit {
+  userIsAdmin$: Observable<User>;
+  constructor(private store: Store<any>) {}
+
+  ngOnInit() {
+    this.userIsAdmin$ = this.store.pipe(select(selectUserIsAdmin));
+    if (this.userIsAdmin$) {
+      console.log(this.userIsAdmin$);
+    }
+  }
+}
 
 @Component({
   selector: 'welcome',
   templateUrl: 'welcome.component.html'
 })
-export class WelcomeDialog implements OnInit {
-  userIsAdmin$: Observable<User>;
-
-  constructor(public dialog: MatDialog, private store: Store<any>) {}
+export class WelcomeDialog {
+  constructor(public dialog: MatDialog) {}
 
   openDialog() {
     const dialogRef = this.dialog.open(WelcomeComponent, { panelClass: 'custom-dialog-container' });
@@ -27,11 +35,5 @@ export class WelcomeDialog implements OnInit {
     dialogRef.afterClosed().subscribe(result => {
       console.log(`Dialog result: ${result}`);
     });
-  }
-  ngOnInit() {
-    this.userIsAdmin$ = this.store.pipe(select(selectUserIsAdmin));
-    if (this.userIsAdmin$) {
-      console.log(`check result: /json, stringify/ ${this.userIsAdmin$}`);
-    }
   }
 }
