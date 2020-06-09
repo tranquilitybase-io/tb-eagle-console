a954679570f057d7c9b81b28aef019d7477f9832
Rafal Mucha <r.p.mucha@gmail.com>
2020-06-09 17:38:13 +0200

    Test fix - mockStore added

--- src/app/shared/welcome/welcome.component.spec.ts
+++ src/app/shared/welcome/welcome.component.spec.ts
@@ -1,14 +1,17 @@
 import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { WelcomeComponent } from './welcome.component';
+import { provideMockStore, MockStore } from '@ngrx/store/testing';
 
 describe('WelcomeComponent', () => {
   let fixture: ComponentFixture<WelcomeComponent>;
   let component: WelcomeComponent;
+  let store: MockStore<any>;
+  const initialState = true;
   beforeEach(() => {
     TestBed.configureTestingModule({
       schemas: [NO_ERRORS_SCHEMA],
-      providers: [],
+      providers: [provideMockStore({ initialState })],
       declarations: [WelcomeComponent]
     });
 
