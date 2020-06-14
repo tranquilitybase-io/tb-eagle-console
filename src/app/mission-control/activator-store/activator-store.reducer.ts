9e778453cf034c1a16cca1f5e97f163d5ab31491
Fábio Andrey Salles <fyss@gft.com>
2020-06-14 20:08:29 -0300

    BUG -ActivatorStore is not applying status changes
     - #172

--- src/app/mission-control/activator-store/activator-store.reducer.ts
+++ src/app/mission-control/activator-store/activator-store.reducer.ts
@@ -1,15 +1,22 @@
 import { createReducer, createSelector, on } from '@ngrx/store';
 
-import { setProgress, setCategoriesCount, setActivatorsCount } from './activator-store.actions';
+import {
+  setProgress,
+  setCategoriesCount,
+  setActivatorsCount,
+  setActivatorsByCategoryData
+} from './activator-store.actions';
+import { Activator } from './activator-store.model';
 
 export const featureKey = 'activator-store';
 
-const initialState = { step: 0 };
+const initialState = { step: 0, activatorsByCategoryData: [] };
 const innerReducer = createReducer(
   initialState,
-  on(setProgress, (state, { step }) => ({ ...state, step })),
+  on(setActivatorsByCategoryData, (state, { activatorsByCategoryData }) => ({ ...state, activatorsByCategoryData })),
+  on(setActivatorsCount, (state, { activatorsCount }) => ({ ...state, activatorsCount })),
   on(setCategoriesCount, (state, { categoriesCount }) => ({ ...state, categoriesCount })),
-  on(setActivatorsCount, (state, { activatorsCount }) => ({ ...state, activatorsCount }))
+  on(setProgress, (state, { step }) => ({ ...state, step }))
 );
 
 export const reducer = (state, action) => {
@@ -20,3 +27,6 @@ export const selectFeature = state => state[featureKey];
 export const selectProgress = createSelector(selectFeature, ({ step }) => step);
 export const selectCategoriesCount = createSelector(selectFeature, ({ categoriesCount }) => categoriesCount);
 export const selectActivatorsCount = createSelector(selectFeature, ({ activatorsCount }) => activatorsCount);
+export const selectActivatorsByCategoryData = createSelector(selectFeature, state =>
+  state ? state.activatorsByCategoryData : ([] as Activator[])
+);
