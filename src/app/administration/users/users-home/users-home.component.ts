4bdbd78f962e315bbf75702789d0c64812ea5ff8
kwicherska <katarzyna.wicherska@gft.com>
2020-06-23 16:09:05 +0200

    initial setup, model + users-home

---
+++ src/app/administration/users/users-home/users-home.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-users-home',
+  templateUrl: './users-home.component.html',
+  styleUrls: ['./users-home.component.scss']
+})
+export class UsersHomeComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
