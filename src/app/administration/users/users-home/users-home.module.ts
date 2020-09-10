cc1e2d15688b0081756517c4e98ffd460c732ada
Fábio Andrey Salles <fyss@gft.com>
2020-09-10 18:23:50 -0300

    #190 Review

--- src/app/administration/users/users-home/users-home.module.ts
+++ src/app/administration/users/users-home/users-home.module.ts
@@ -6,24 +6,22 @@ import { Routes, RouterModule } from '@angular/router';
 import { UsersHomeComponent } from './users-home.component';
 import { UsersHomeGridCardComponent } from './users-home-grid/users-home-grid-card/users-home-grid-card.component';
 import { UsersHomeGridComponent } from './users-home-grid/users-home-grid.component';
+import { UsersHomeListComponent } from './users-home-list/users-home-list.component';
+import { UsersHomeSelectComponent } from './users-home-select/users-home-select.component';
 
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatChipsModule } from '@angular/material/chips';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 import { MatListModule } from '@angular/material/list';
 import { MatMenuModule } from '@angular/material/menu';
-import { UsersHomeListComponent } from './users-home-list/users-home-list.component';
-import { UsersHomeSelectComponent } from './users-home-select/users-home-select.component';
-import {
-  MatFormFieldModule,
-  MatPaginatorModule,
-  MatTableModule,
-  MatInputModule,
-  MatSortModule,
-  MatTooltipModule
-} from '@angular/material';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+import { MatTableModule } from '@angular/material/table';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 const routes: Routes = [
   {
@@ -47,15 +45,15 @@ const routes: Routes = [
     MatButtonModule,
     MatCardModule,
     MatChipsModule,
+    MatFormFieldModule,
     MatGridListModule,
     MatIconModule,
+    MatInputModule,
     MatListModule,
     MatMenuModule,
-    MatFormFieldModule,
     MatPaginatorModule,
-    MatTableModule,
-    MatInputModule,
     MatSortModule,
+    MatTableModule,
     MatTooltipModule
   ]
 })
