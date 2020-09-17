4911b0a00a37b3b3015b34c0bab7c52934866b9d
Tadeusz Miesiac <tzmc@gft.com>
2020-09-17 13:24:21 +0200

    validate by url

--- src/app/mission-control/activator-store/activator-store-dialog/activator-store-dialog-create/activator-store-dialog-create.component.ts
+++ src/app/mission-control/activator-store/activator-store-dialog/activator-store-dialog-create/activator-store-dialog-create.component.ts
@@ -1,8 +1,8 @@
-import { Component, OnInit, Inject } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatDialogRef } from '@angular/material/dialog';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, AbstractControl } from '@angular/forms';
 import { Store } from '@ngrx/store';
-
+import { createActivatorByURL } from '../../activator-store.actions';
 @Component({
   selector: 'app-activator-store-dialog-grant-access',
   templateUrl: './activator-store-dialog-create.component.html',
@@ -10,6 +10,8 @@ import { Store } from '@ngrx/store';
 })
 export class ActivatorStoreDialogCreateComponent implements OnInit {
   createActivatorForm: FormGroup;
+  URLPattern =
+    '((([A-Za-z]{3,9}:(?://)?)(?:[-;:&=+$,w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=+$,w]+@)[A-Za-z0-9.-]+)((?:/[+~%/.w-_]*)???(?:[-+=&;%@.w_]*)#?(?:[w]*))?)';
 
   constructor(
     private formBuilder: FormBuilder,
@@ -19,17 +21,14 @@ export class ActivatorStoreDialogCreateComponent implements OnInit {
 
   ngOnInit() {
     this.createActivatorForm = this.formBuilder.group({
-      url: ['', Validators.required]
+      url: ['', [Validators.required]]
     });
   }
 
-  isFieldValid(field: string) {
-    return this.createActivatorForm.get(field).touched && !this.createActivatorForm.get(field).valid;
-  }
-
   createActivator() {
+    console.log(this.createActivatorForm);
     if (this.createActivatorForm.valid) {
-      //this.store.dispatch(grantAccess({ activatorId: this.data.activatorId, teamId }));
+      this.store.dispatch(createActivatorByURL({ url: this.createActivatorForm.value.url }));
     } else {
       this.createActivatorForm.markAllAsTouched();
     }
