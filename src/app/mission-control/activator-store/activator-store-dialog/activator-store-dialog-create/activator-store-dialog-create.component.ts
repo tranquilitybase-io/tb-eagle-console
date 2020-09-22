713f210ee1ad67eba04aca31fc5b1b305f1a8898
Tadeusz Miesiac <tzmc@gft.com>
2020-09-22 12:29:36 +0200

    close dialog on step one

--- src/app/mission-control/activator-store/activator-store-dialog/activator-store-dialog-create/activator-store-dialog-create.component.ts
+++ src/app/mission-control/activator-store/activator-store-dialog/activator-store-dialog-create/activator-store-dialog-create.component.ts
@@ -3,6 +3,7 @@ import { MatDialogRef } from '@angular/material/dialog';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Store, select } from '@ngrx/store';
 import { createActivatorByURL } from '../../activator-store.actions';
+import { selectActivatorData } from '../../activator-store.reducer';
 
 @Component({
   selector: 'app-activator-store-dialog-grant-access',
@@ -13,12 +14,24 @@ export class ActivatorStoreDialogCreateComponent implements OnInit {
   createActivatorForm: FormGroup;
   URLPattern =
     '((([A-Za-z]{3,9}:(?://)?)(?:[-;:&=+$,w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=+$,w]+@)[A-Za-z0-9.-]+)((?:/[+~%/.w-_]*)???(?:[-+=&;%@.w_]*)#?(?:[w]*))?)';
+  activatorId = 0;
+  wasOpened = false;
 
   constructor(
     private formBuilder: FormBuilder,
     private store: Store<any>,
     private dialogRef: MatDialogRef<ActivatorStoreDialogCreateComponent>
-  ) {}
+  ) {
+    this.store.pipe(select(selectActivatorData)).subscribe(activatorData => {
+      if (!this.wasOpened) {
+        this.activatorId = activatorData.id;
+        this.wasOpened = true;
+      }
+      if (activatorData.id && this.activatorId !== activatorData.id) {
+        this.dialogRef.close();
+      }
+    });
+  }
 
   ngOnInit() {
     this.createActivatorForm = this.formBuilder.group({
