97ea296c2a11d2e639de3be246a47bc735a1b10d
Fábio Andrey Salles <fyss@gft.com>
2021-05-04 09:34:41 -0300

    #506 Review

--- src/app/mission-control/activator-store/activator-store-home/activator-store-home.module.ts
+++ src/app/mission-control/activator-store/activator-store-home/activator-store-home.module.ts
@@ -14,11 +14,14 @@ import { ActivatorStoreHomeListFilterComponent } from './activator-store-home-li
 
 import { ActivatorStoreDialogModule } from '../activator-store-dialog/activator-store-dialog.module';
 
+import { LayoutModule } from '@angular/cdk/layout';
 import { MatButtonModule } from '@angular/material/button';
 import { MatButtonToggleModule } from '@angular/material/button-toggle';
 import { MatCardModule } from '@angular/material/card';
 import { MatChipsModule } from '@angular/material/chips';
 import { MatDialogModule } from '@angular/material/dialog';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatExpansionModule } from '@angular/material/expansion';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatIconModule } from '@angular/material/icon';
@@ -31,10 +34,6 @@ import { MatSortModule } from '@angular/material/sort';
 import { MatStepperModule } from '@angular/material/stepper';
 import { MatTableModule } from '@angular/material/table';
 import { MatTooltipModule } from '@angular/material/tooltip';
-
-import { LayoutModule } from '@angular/cdk/layout';
-import { MatDividerModule } from '@angular/material/divider';
-import { MatExpansionModule } from '@angular/material/expansion';
 
 const routes: Routes = [
   {
@@ -60,11 +59,14 @@ const routes: Routes = [
     SharedModule,
     ActivatorStoreDialogModule,
     RouterModule.forChild(routes),
+    LayoutModule,
     MatButtonModule,
     MatButtonToggleModule,
     MatCardModule,
     MatChipsModule,
     MatDialogModule,
+    MatDividerModule,
+    MatExpansionModule,
     MatFormFieldModule,
     MatGridListModule,
     MatIconModule,
@@ -77,9 +79,6 @@ const routes: Routes = [
     MatStepperModule,
     MatTableModule,
     MatTooltipModule,
-    MatDividerModule,
-    LayoutModule,
-    MatExpansionModule,
   ],
 })
 export class ActivatorStoreHomeModule {}
